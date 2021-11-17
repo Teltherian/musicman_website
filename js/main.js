d9f248bf9e5a18ef613c98f17c2fd27438cba18e
Teltherian <timhaunsperger@gmail.com>
2021-11-17 14:57:18 -0600

    changed to https

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const url = "http://72.177.61.58:5000/"
+const url = "https://72.177.61.58:5000/"
 
 const GET = async (src) => {
     let data = await fetch(url + src, {headers: {'Content-Type': 'application/json'}})
